b4c49a17e279fe54ce2ce451b314093630a1eeb7
jhon freddy tavera <jhon.tavera@elite.edu.co>
2022-12-27 21:04:37 -0500

    este es el primer commit del proyecto

---
+++ hello.py
@@ -0,0 +1,10 @@
+print('hola a todos como estan')
+
+hello = {
+    'nico ': 'hola como estas ',
+    'emanuel': 'hola como estas'
+}
+
+print(hello)
+print(hello)
+print('los estraño mucho chicos ojala nos podemos ver ')
